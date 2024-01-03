5450d0cf28a1b9e2a75ff862848d49e1475960f5
hz12opensource <hz12opensource@gmail.com>
2024-01-02 23:14:34 -0600

    add overclock script

--- collect.sh
+++ collect.sh
@@ -1,5 +1,5 @@
 cd plutosdr-fw_0.38_libre
-git add -N scripts/libre.its scripts/libre.mk
+git add -N scripts/libre.its scripts/libre.mk overclock.sh
 git diff --ignore-submodules > ../patches/fw.diff
 cd hdl
 git add -N projects/libre/system_bd.tcl projects/libre/Makefile projects/libre/system_constr.xdc projects/libre/system_project.tcl projects/libre/system_top.v
